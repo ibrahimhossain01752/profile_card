e478b01117bd2dc4d82c02b459a811ff873ac2df
ibrahimhossain01752 <ibrahimhossain01752@gmail.com>
2023-07-05 20:00:54 +0600

    profile_Card

---
+++ app.js
@@ -0,0 +1,21 @@
+const postsData = [
+    {
+      id: 1,
+      username: "John Doe",
+      time:"2 hours ago",
+      profile: "https://bootdey.com/img/Content/avatar/avatar1.png",
+      content: "You can never get enough of these magical views from Swizerland.",
+      images: [
+        { id: 1, url: "" },
+        { id: 2, url: "" },
+        {id: 3, url: "" },
+      ],
+    },
+]
+
+
+
+new Vue({
+    el: '#app'
+  });
+  
